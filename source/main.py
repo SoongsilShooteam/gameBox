8b0b8d5b0917c0641960266a8d48a640cbd54ccd
ggomdyu <ggomdyu@gmail.com>
2020-09-29 19:59:43 +0900

    주석 추가함

--- source/main.py
+++ source/main.py
@@ -15,6 +15,9 @@ isPlay = False
 # 프로세스의 현재 경로를 루트로 변경함.
 #os.chdir("../")
 
+
+#$#@%#%#%^#%#^#$^$#%^$%^
+
 titleScene = titleScene.TitleScene(screen) #타이틀 화면 생성
 stageOneScene = stageOneScene.StageOneScene() #스테이지 1 생성
 scene = titleScene #처음 화면을 타이틀 화면으로 고정
